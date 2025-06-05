f4b8f8b2526e5d9352b9e883b9fe470095c8f046
Charlie Stevens <charliestevens@MacBookPro.fios-router.home>
2025-06-04 20:06:23 -0400

    answers will now be marked correct if the users enters a person's last name

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import spacy
 
 app = FastAPI()
 
+# load in the pre-trained spacy model
+nlp = spacy.load('en_core_web_sm')
 
 # function to preprocess a string of text
 # -- First NLP Step --
@@ -32,6 +34,17 @@ def preprocess(text: str) -> list[str]:
     # Now return the preprocessed list of words
     return words
 
+# function to determine if a string is a person or not
+def is_person(text: str) -> bool:
+    doc = nlp(text)
+    for e in doc.ents:
+        # if any tag is a person, then we will assume the entity is a person
+        # this is safe to assume because of the specific condition in which we
+        # are calling this function
+        if e.label_ == "PERSON":
+            return True
+    return False
+
 
 # function to call in the api route
 # returns whether the user response is correct
@@ -43,15 +56,29 @@ def is_correct(user_response: str, correct_response: str) -> bool:
     # if these lists are exactly the same, then the user response is correct
     if user_words == correct_words:
         return True
+
+    # join both lists back into strings
+    user_string = " ".join(user_words)
+    correct_string = " ".join(correct_words)
     
     # Next check if the correct response is a person's name -- if so we can check if
     # the user just entered that person's last name, and it will be marked correct
 
-    if len(user_words) == 1:
-        # this must be the case if the user only entered a person's last name
-        # if the user entered the full name, it would be caught in the check above
-        pass
+    # CONDITIONS:
+    # 1) len(user_words) == 1 -> user only passed in the last name of a person
+    # 2) len(correct_words) <= 2 -> correct answer may be of the form (first name, last name)
+    # 3) is_person() -> check if the correct answer is a person
+    if len(user_words) == 1 and len(correct_words) <= 2 and is_person(correct_string):
+        # if the user entered the correct last name - the response is correrct
+        if user_words[0] == correct_words[-1]:
+            return True
+
+    # NOTE: If the answer is a person who has a name with more than 2 words
+    # (i.e. Vincent van Gogh), the user must enter the full name
+    # This could be a source of improvement in the future
 
     # Finally, apply comparison/similarity techniques to determine how similar the user's
     # response is to the correct response -- this will allow the program to be less strict
     # in regards to typos and misspellings
+
+
